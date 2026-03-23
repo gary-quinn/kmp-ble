b761d1ff8f68de5661557c3ceddb4d9d4b60bf89
Gary <gary@atruedev.com>
2026-03-23 23:33:19 +0200

    fix: update outdated GitHub repository URL
    Replace all references from atruedeveloper/kmp-ble to gary-quinn/kmp-ble
    across docs, build configs, CI workflows, and Package.swift.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KmpBle",
-            url: "https://github.com/atruedeveloper/kmp-ble/releases/download/v0.3.3/KmpBle.xcframework.zip",
+            url: "https://github.com/gary-quinn/kmp-ble/releases/download/v0.3.3/KmpBle.xcframework.zip",
             checksum: "98aa5a0d761d3f30773bffe7913fe73e75263dda0a697bd2b02aa03d5eac8317"
         ),
     ]
