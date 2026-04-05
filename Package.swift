8f805406dae32125dc4b98d4c3004dc7f89ddddf
Gary Quinn <gary@atruedev.com>
2026-04-05 19:01:06 +0000

    update Package.swift for v0.3.13

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KmpBle",
-            url: "https://github.com/gary-quinn/kmp-ble/releases/download/v0.3.12/KmpBle.xcframework.zip",
-            checksum: "f85ebaf39383ceb88b1e42a73f6c382ff98fbcb9d02070b46aab0072855aa4fe"
+            url: "https://github.com/gary-quinn/kmp-ble/releases/download/v0.3.13/KmpBle.xcframework.zip",
+            checksum: "15685c65c84b5c0ec428310d1dec915e9d89feaf8e3fc9b9a34f0d7b512e6711"
         ),
     ]
 )
