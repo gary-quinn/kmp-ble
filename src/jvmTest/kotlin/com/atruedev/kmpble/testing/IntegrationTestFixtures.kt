a5fafa3dbf518fd566abdbaf423ccf28c24594ae
Gary <gary@atruedev.com>
2026-06-12 12:51:48 +0300

    fix: move IntegrationTestFixtures and IntegrationTestScanner to jvmTest
    Required for BleIntegrationJvmTest compilation.

---
+++ src/jvmTest/kotlin/com/atruedev/kmpble/testing/IntegrationTestFixtures.kt
@@ -0,0 +1,17 @@
+package com.atruedev.kmpble.testing
+
+import com.atruedev.kmpble.testing.FakeScanner
+import com.atruedev.kmpble.testing.FakeScannerBuilder
+import kotlin.uuid.ExperimentalUuidApi
+
+@OptIn(ExperimentalUuidApi::class)
+object IntegrationTestFixtures {
+    val scanner: FakeScanner = FakeScannerBuilder().apply {
+        advertisement {
+            identifier("AA:BB:CC:DD:EE:FF")
+            name("IntegrationTestDevice")
+            rssi(-55)
+            serviceUuids("180d")
+        }
+    }.build()
+}
