fa93187090636bc3b9d47340dc27c6263555b017
Gary <gary@atruedev.com>
2026-06-30 03:48:50 +0300

    feat(dx): add Peripheral.reconnect() convenience for single-tap reconnection (#522)
    - Add lastConnectionOptions property to Peripheral interface
    - Add reconnect() extension that reuses last-used ConnectionOptions
    - Auto re-discovers services after reconnection
    - Throws IllegalStateException when connect() never called or
      already connected
    - Implements lastConnectionOptions in AndroidPeripheral, IosPeripheral,
      FakePeripheral, and StubPeripheral
    
    Closes #522

--- src/androidMain/kotlin/com/atruedev/kmpble/peripheral/AndroidPeripheral.kt
+++ src/androidMain/kotlin/com/atruedev/kmpble/peripheral/AndroidPeripheral.kt
@@ -93,6 +93,9 @@ public class AndroidPeripheral internal constructor(
     override val mtu: StateFlow<Int> get() = peripheralContext.mtu
     override val dataLengthParameters: StateFlow<DataLengthParameters?> get() = peripheralContext.dataLengthParameters
 
+    private var _lastConnectionOptions: ConnectionOptions? = null
+    override val lastConnectionOptions: ConnectionOptions? get() = _lastConnectionOptions
+
     private val _closed = atomic(false)
     internal val closed: Boolean get() = _closed.value
 
@@ -135,6 +138,7 @@ public class AndroidPeripheral internal constructor(
 
     @OptIn(ExperimentalBleApi::class)
     override suspend fun connect(options: ConnectionOptions) {
+        _lastConnectionOptions = options
         connectInternal(options)
     }
 
